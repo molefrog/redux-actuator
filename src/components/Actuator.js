65a55495ef428819943be682460fd5d5af5a71d6
Alexey Taktarov <molefrog@gmail.com>
2017-12-06 03:02:26 +0300

    Do not rely on react-redux's connect. Use store via the context.

--- src/components/Actuator.js
+++ src/components/Actuator.js
@@ -2,24 +2,88 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { connect } from 'react-redux'
-import ActuatorInner from './ActuatorInner'
+import { currentTimestamp } from '../utils/currentTimestamp'
 
-const Actuator = ({ event, onTrigger, ...props }) => (
-  <ActuatorInner event={event} handler={onTrigger} {...props} />
-)
+// Actuator is an auxiliary component used internally
+// by Actuator component.
+// It has no connection to the store, it only responds to an event
+// change passed as a prop.
+export class Actuator extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.onStateChanged = this.onStateChanged.bind(this)
+  }
 
-const mapStateToProps = (state, ownProps) => {
-  const channels = state.actuator || {}
-  const event = channels[ownProps.channel]
+  onStateChanged() {
+    const event = this.getEvent(this.context.store)
+    event && this.checkEvent(event)
+  }
 
-  return { event }
+  getEvent(store) {
+    const state = store.getState()
+
+    if (!state.actuator) return null
+    return state.actuator[this.props.channel]
+  }
+
+  checkEvent(event, options = {}) {
+    // Compare event keys (they have to be unique)
+    if (this._prevKey && this._prevKey === event.key) {
+      return
+    }
+
+    this._prevKey = event.key
+
+    // Special case when the event was dispatched before
+    // the mount. If you need to handle cases like
+    // this one, pass `deltaError` prop, which literally means:
+    // "handle missed events on mount if it's younger than `deltaError`"
+    let shouldHandleOnMount = false
+    const { deltaError } = this.props
+
+    if (options.onMount && deltaError) {
+      const now = currentTimestamp()
+      const delta = Math.abs(now - event.timestamp)
+
+      shouldHandleOnMount = delta <= deltaError
+    }
+
+    if (options.onMount && !shouldHandleOnMount) {
+      return
+    }
+
+    this.props.onTrigger(...event.args)
+  }
+
+  componentDidMount() {
+    const store = this.context.store
+
+    if (store) {
+      this._sub = store.subscribe(this.onStateChanged)
+      const event = this.getEvent(store)
+
+      event && this.checkEvent(event, { onMount: true })
+    }
+  }
+
+  componentWillUnmount() {
+    this._sub && this._sub()
+  }
+
+  render() {
+    if (!this.props.children) return null
+    return this.props.children
+  }
 }
 
-const WrappedActuator = connect(mapStateToProps)(Actuator)
-
-WrappedActuator.propTypes = {
+Actuator.propTypes = {
   channel: PropTypes.string.isRequired,
-  onTrigger: PropTypes.func
+  onTrigger: PropTypes.func.isRequired,
+  deltaError: PropTypes.number
 }
 
-export default WrappedActuator
+Actuator.contextTypes = {
+  store: PropTypes.object
+}
+
+export default Actuator
